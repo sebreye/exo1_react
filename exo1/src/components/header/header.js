f0348dd942ebffcab8d150574786763092e3ced7
sebastian Reyes <sebreyes222gmail.com>
2023-02-06 15:45:40 +0100

    ajout active

--- exo1/src/components/header/header.js
+++ exo1/src/components/header/header.js
@@ -6,19 +6,23 @@ import Clock from './public/img/icon-clock.svg'
 import Bot from './public/img/image-avatar.png'
 import View from './public/img/icon-view.svg'
 const Header = ()=>{
+
     return(
         <div className='cards-header'>
-            <img src={Img} alt="" width={350} style={{borderRadius:"15px"}}/>
-            <img src={View} alt="" className='view-icon'/>
-            <div>
-                <h2>Equilibrium</h2>
-                <p>Our Equilibrium collection promotes balances and calm</p>
+            
+            <div className='img-hover'>
+                <div className='div-icon'><img src={View} alt="" className='view-icon'/></div>
+            </div>
+            
+            <div className='Para-header'>
+                <h2>Equilibrium #3429</h2>
+                <p>Our Equilibrium collection promotes  balances and calm</p>
                 <div className='info-crypt'>
                     <p style={{color: "aqua"}}><img src={Eth} alt="" /> 0.041 ETH</p>
                     <p><img src={Clock} alt="" /> 3 days left</p>
                 </div>
                 <hr />
-                <p><img src={Bot} alt="" width={50} style={{border: "1px solid white", borderRadius: "50%"}} /> Creation of <span className='Jules'>Jules Richards</span></p>
+                <p><img src={Bot} alt="" width={30} style={{border: "1px solid white", borderRadius: "50%"}} /> Creation of <span className='Jules'>Jules Richards</span></p>
             </div>
         </div>
     )
